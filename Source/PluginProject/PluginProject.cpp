074eb785d165354175c40c3bc8ec220f00fc6164
Zizin13 <162662805+Zizin13@users.noreply.github.com>
2024-09-23 15:25:14 -0700

    blank ue5 project

---
+++ Source/PluginProject/PluginProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PluginProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PluginProject, "PluginProject" );
