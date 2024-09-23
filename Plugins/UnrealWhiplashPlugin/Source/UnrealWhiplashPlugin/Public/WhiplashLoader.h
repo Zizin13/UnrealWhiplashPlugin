53ecc9f3452ae263b3460f111bcbb3f5f2da8f6e
Zizin13 <162662805+Zizin13@users.noreply.github.com>
2024-09-23 16:29:03 -0700

    empty blueprint function library

---
+++ Plugins/UnrealWhiplashPlugin/Source/UnrealWhiplashPlugin/Public/WhiplashLoader.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "WhiplashLoader.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALWHIPLASHPLUGIN_API UWhiplashLoader : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
